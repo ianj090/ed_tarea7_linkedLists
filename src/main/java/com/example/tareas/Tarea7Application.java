8dcac344c1c008274816703514dea3b61043d3e8
Ian <ianjenatz@ufm.edu>
2020-02-20 07:36:24 -0600

    first

---
+++ src/main/java/com/example/tareas/Tarea7Application.java
@@ -0,0 +1,13 @@
+package com.example.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea7Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/tareas/tarea7.java
@@ -0,0 +1,131 @@
+package com.example.tareas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class tarea7 {
+
+    @RequestMapping("/list")
+    public String print() {
+        return LinkedList.listSongs();
+    }
+
+    @RequestMapping("/add")
+    public String add(String song) {
+        return LinkedList.addSong(song);
+    }
+
+    @RequestMapping("/play")
+    public String play() {
+        return LinkedList.playNow();
+    }
+
+    @RequestMapping("/next")
+    public String next() {
+        return LinkedList.playNext();
+    }
+
+    @RequestMapping("/previous")
+    public String prev() {
+        return LinkedList.playPrev();
+    }
+}
+
+class LinkedList {
+
+    static LinkedList list = new LinkedList();
+    Node head;
+    static Node playing;
+
+    static class Node {
+        String data;
+        Node next;
+        Node prev;
+
+        Node(String data_val) {
+            prev = null;
+            data = data_val;
+            next = null;
+        }
+    }
+
+    public static String listSongs() {
+        Node current = list.head;
+        String result = "";
+
+        while (current != null) {
+            result = result + current.data + "<br>";
+            current = current.next;
+        }
+        if (result.equals("")) {
+            return "No songs added";
+        }
+        return result;
+    }
+
+    public static String addSong(String name) {
+        Node new_n = new Node(name);
+        new_n.next = null;
+        if (list.head == null) {
+            list.head = new_n;
+        }
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            new_n.prev = last;
+            last.next = new_n;
+        }
+        return "Added Successfully";
+    }
+
+    public static String playNow() {
+        if (playing == null) {
+            playing = list.head;
+        }
+        if (playing == null) {
+            return "No songs added";
+        }
+        return "Playing: " + playing.data;
+    }
+
+//    public static String playNow() {
+//        if (playing == null) {
+//            playing = list.head;
+//        }
+//        if (playing == null) {
+//            return "No songs added";
+//        }
+//        return "Playing: " + playing.data;
+//    }
+
+    public static String playNext() {
+        if (playing == null) {
+            playing = list.head;
+        }
+        if (playing == null) {
+            return "No songs added";
+        }
+        playing = playing.next;
+        if (playing == null) {
+            return "No more songs";
+        }
+        return "Playing: " + playing.data;
+    }
+
+    public static String playPrev() {
+        if (playing == null) {
+            playing = list.head;
+        }
+        if (playing == null) {
+            return "No songs added";
+        }
+        playing = playing.prev;
+        if (playing == null) {
+            return "No more songs";
+        }
+        return "Playing: " + playing.data;
+    }
+}
